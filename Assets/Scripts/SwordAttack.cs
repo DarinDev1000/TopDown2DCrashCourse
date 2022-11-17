b4ef30bbd008e77ba88454c6b8316c7e135b1ee5
Darin Crawmer <darindev1000@gmail.com>
2022-11-17 04:06:32 -0800

    wip attack collision

--- Assets/Scripts/SwordAttack.cs
+++ Assets/Scripts/SwordAttack.cs
@@ -8,11 +8,14 @@ public class SwordAttack : MonoBehaviour
     public float damage = 1;
     public float timeBetweenAttacks = 0.5f;
     private float attackCooldown = 0f;
+    private bool isCollidingWithEnemy = false;
+    private Enemy collidingEnemy;
 
     Vector2 attackOffsetX;
 
      // Start is called before the first frame update
     private void Start() {
+        swordCollider = GetComponent<Collider2D>();
         attackOffsetX = transform.localPosition;
     }
 
@@ -21,35 +24,76 @@ public class SwordAttack : MonoBehaviour
     }
 
     public void AttackRight() {
-        swordCollider.enabled = true;
+        // swordCollider.enabled = true;
         transform.localPosition = attackOffsetX;
+        DoAttack();
     }
 
     public void AttackLeft() {
-        swordCollider.enabled = true;
+        // swordCollider.enabled = true;
         transform.localPosition = new Vector3(attackOffsetX.x * -1, attackOffsetX.y);
+        DoAttack();
     }
     public void StopAttack() {
-        swordCollider.enabled = false;
+        // swordCollider.enabled = false;
     }
 
-    // private void OnTriggerStay2D(Collider2D other) {
-    private void OnTriggerEnter2D(Collider2D other) {
-        print("OnTriggerStay2D");
-        print(attackCooldown >= timeBetweenAttacks);
-        print(other.tag);
-        // If longer than attack cooldown, attack
-        if (attackCooldown >= timeBetweenAttacks) {
-            attackCooldown = 0f;
-            if (other.tag == "Enemy") {
-                print(attackCooldown);
-                print(timeBetweenAttacks);
-                // Deal damage to enemy
-                Enemy enemy = other.GetComponent<Enemy>();
+//  // private void OnTriggerStay2D(Collider2D other) {
+//     private void OnTriggerEnter2D(Collider2D other) {
+//         print("OnTriggerStay2D");
+//         print(attackCooldown >= timeBetweenAttacks);
+//         print(other.tag);
+//         // If longer than attack cooldown, attack
+//         if (attackCooldown >= timeBetweenAttacks) {
+//             attackCooldown = 0f;
+//             if (other.tag == "Enemy") {
+//                 print(attackCooldown);
+//                 print(timeBetweenAttacks);
+//                 // Deal damage to enemy
+//                 Enemy enemy = other.GetComponent<Enemy>();
 
-                if (enemy != null) {
-                    enemy.Health -= damage;
-                }
+//                 if (enemy != null) {
+//                     enemy.Health -= damage;
+//                 }
+//             }
+//         }
+//     }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        // print("OnTriggerEnter2D");
+        if (other.tag == "Enemy") {
+            Enemy enemy = other.GetComponent<Enemy>();
+            collidingEnemy = enemy;
+            if (enemy != null) {
+                isCollidingWithEnemy = true;
+            }
+        } else {
+            Physics2D.IgnoreCollision(other, swordCollider);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        // print("OnTriggerExit2D");
+        if (other.tag == "Enemy") {
+            Enemy enemy = other.GetComponent<Enemy>();
+            if (enemy != null) {
+                isCollidingWithEnemy = false;
+            }
+        }
+    }
+
+    // public void OnCollisionEnter(Collider2D collision) {
+    //     print(collision.tag);
+    //     Physics2D.IgnoreCollision(collision, swordCollider);
+    // }
+
+    private void DoAttack() {
+        if (CheckAttackCooldown()) {
+            attackCooldown = 0f;
+            print(isCollidingWithEnemy);
+            print(collidingEnemy);
+            if (isCollidingWithEnemy && collidingEnemy != null) {
+                collidingEnemy.Health -= damage;
             }
         }
     }
