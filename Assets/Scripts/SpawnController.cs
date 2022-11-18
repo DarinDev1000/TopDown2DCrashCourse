2dbce64fe936e05b8c2df87f13ea09787a1a731c
Darin Crawmer <darindev1000@gmail.com>
2022-11-17 16:21:29 -0800

    fix slime spawn on FixedUpdate spawn slimes as child

--- Assets/Scripts/SpawnController.cs
+++ Assets/Scripts/SpawnController.cs
@@ -7,13 +7,16 @@ public class SpawnController : MonoBehaviour
     // Drag a prefab in unity onto this field
     public GameObject slimePrefab;
     public int spawnSpeed = 10;
+    public float spawnRepeatRate = 1f;
     public int spawnRange = 2;
     public int maxNumber = 20; // TODO: Do this
+
+    private readonly System.Random rnd = new();
 
     // Start is called before the first frame update
     void Start()
     {
-
+        InvokeRepeating(nameof(SpawnSlime), 0, spawnRepeatRate);
     }
 
     // Update is called once per frame
@@ -22,16 +25,22 @@ public class SpawnController : MonoBehaviour
 
     }
 
-    private void FixedUpdate()
+    private void SpawnSlimeRandomChance()
     {
         // print("Fixed update on SpawnController");
-        System.Random rnd = new();
-        int number = rnd.Next(0, 1000);
+        int number = rnd.Next(0, 100);
         if (number < spawnSpeed)
         {
-            float randX = rnd.Next(-spawnRange * 100, spawnRange * 100) * 0.01f;
-            float randY = rnd.Next(-spawnRange * 100, spawnRange * 100) * 0.01f;
-            Instantiate(slimePrefab, new Vector3(randX, randY, 0), Quaternion.identity);
+            SpawnSlime();
         }
+    }
+
+    private void SpawnSlime()
+    {
+        print(GameObject.FindGameObjectsWithTag("Enemy").Length);
+        float randX = rnd.Next(-spawnRange * 100, spawnRange * 100) * 0.01f;
+        float randY = rnd.Next(-spawnRange * 100, spawnRange * 100) * 0.01f;
+        GameObject newSlime = Instantiate(slimePrefab, new Vector3(randX, randY, 0), Quaternion.identity);
+        newSlime.transform.parent = GameObject.Find("SlimeGroup").transform;
     }
 }
